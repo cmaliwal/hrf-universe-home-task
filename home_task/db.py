5fb057ad74faf9643d49acaba87e6c395091f3c8
cmaliwal <chiragmaliwal1995@gmail.com>
2025-07-02 13:47:46 +0530

    Minor changes for sessionmaker

--- home_task/db.py
+++ home_task/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 engine = create_engine("postgresql+psycopg2://admin:adm1n_password@localhost/home_task",)
 pg_session_factory = sessionmaker(
-    engine, Session, autocommit=False, autoflush=False, expire_on_commit=False
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False
 )
 SessionFactory = scoped_session(pg_session_factory)
 
